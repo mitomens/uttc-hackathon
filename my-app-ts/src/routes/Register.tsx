4c330c277b89d8e0e56d766a95d3215fd1696389
keitomitome <keitokf6@g.ecc.u-tokyo.ac.jp>
2023-06-10 01:50:35 +0900

    test console

--- my-app-ts/src/routes/Register.tsx
+++ my-app-ts/src/routes/Register.tsx
@@ -12,7 +12,7 @@ import { Avatar } from '@mantine/core';
 const Register = () => {
   const [registerEmail, setRegisterEmail] = useState<string>("");
   const [registerPassword, setRegisterPassword] = useState<string>("");
-    const [userId, setUserId] = useState<string | undefined>("");
+  const [userId, setUserId] = useState<string | undefined>("");
 
   const handleSubmit = async (e :React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -28,6 +28,7 @@ const Register = () => {
     }
 
     setUserId(fireAuth.currentUser?.uid);
+    console.log(userId);
 
     try {
         const result = await fetch("https://uttc-hackathon2-dbofxfl7wq-uc.a.run.app/edit", {
@@ -35,8 +36,9 @@ const Register = () => {
             body: JSON.stringify({
             userid: userId,
             name: "no name",
-            icon: "https://firebasestorage.googleapis.com/v0/b/term3-keito-mitome.appspot.com/o/aikonnnasi.jpeg?alt=media&token=3750b10b-52d4-479d-a79e-e48bb6f1a20e&_gl=1*yp7g36*_ga*MTI1Nzc5ODgwMy4xNjg1NjQ1MDI3*_ga_CW55HF8NVT*MTY4NjMxOTc2OC4xNi4xLjE2ODYzMTk3OTUuMC4wLjA.",
-            }),
+            //icon: "https://firebasestorage.googleapis.com/v0/b/term3-keito-mitome.appspot.com/o/aikonnnasi.jpeg?alt=media&token=3750b10b-52d4-479d-a79e-e48bb6f1a20e&_gl=1*yp7g36*_ga*MTI1Nzc5ODgwMy4xNjg1NjQ1MDI3*_ga_CW55HF8NVT*MTY4NjMxOTc2OC4xNi4xLjE2ODYzMTk3OTUuMC4wLjA.",
+            icon:"no-icon"
+        }),
         });
         if (!result.ok) {
             throw Error(`Failed to create user: ${result.status}`);
