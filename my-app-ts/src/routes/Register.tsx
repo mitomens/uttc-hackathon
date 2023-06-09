a4a739757875ed7fc4d93c77e6b4a1f36bdf447e
keitomitome <keitokf6@g.ecc.u-tokyo.ac.jp>
2023-06-10 02:22:31 +0900

    test await 3

--- my-app-ts/src/routes/Register.tsx
+++ my-app-ts/src/routes/Register.tsx
@@ -12,7 +12,6 @@ import { Avatar } from '@mantine/core';
 const Register = () => {
   const [registerEmail, setRegisterEmail] = useState<string>("");
   const [registerPassword, setRegisterPassword] = useState<string>("");
-  const [userId, setUserId] = useState<string | undefined>("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -33,17 +32,13 @@ const Register = () => {
   
     const currentUser = fireAuth.currentUser;
     if (currentUser) {
-      setUserId(currentUser.uid);
-      console.log(userId);
-      console.log(currentUser.uid);
-  
       try {
         const result = await fetch(
           "https://uttc-hackathon2-dbofxfl7wq-uc.a.run.app/edit",
           {
             method: "POST",
             body: JSON.stringify({
-              userid: userId,
+              userid: currentUser.uid,
               name: "no name",
               //icon: "https://firebasestorage.googleapis.com/v0/b/term3-keito-mitome.appspot.com/o/aikonnnasi.jpeg?alt=media&token=3750b10b-52d4-479d-a79e-e48bb6f1a20e&_gl=1*yp7g36*_ga*MTI1Nzc5ODgwMy4xNjg1NjQ1MDI3*_ga_CW55HF8NVT*MTY4NjMxOTc2OC4xNi4xLjE2ODYzMTk3OTUuMC4wLjA.",
               icon: "no-icon",
